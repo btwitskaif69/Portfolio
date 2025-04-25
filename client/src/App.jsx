66c8cf698ded5a774a949df7afdf8d308b32c5cb
Mohd Kaif <124128116+btwitskaif69@users.noreply.github.com>
2025-04-25 18:26:41 +0530

    minor change

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,14 +2,17 @@ import React from 'react'
 import Navbar from './components/Navbar'
 import Hero from './components/Pages/Home/Hero'
 import { SmoothCursor } from "@/components/ui/smooth-cursor";
+import { VelocityScroll } from "@/components/magicui/scroll-based-velocity";
 
 const App = () => {
   return (
-    <>
+    <div className="flex flex-col space-y-4">
     <SmoothCursor />
     <Navbar/>
     <Hero/>
-    </>
+    <VelocityScroll>Scroll Based Velocity</VelocityScroll>
+
+    </div>
   )
 }
 
