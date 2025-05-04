010dc245a6538fcff3958143fa9d15872d70ef1c
Mohd Kaif <124128116+btwitskaif69@users.noreply.github.com>
2025-05-04 22:45:58 +0530

    added projects

--- client/src/components/projects.js
+++ client/src/components/projects.js
@@ -1,10 +1,7 @@
-import image1 from '../assets/image1.jpg';
-import image2 from '../assets/image2.jpg';
-import image3 from '../assets/image3.jpg';
-import image4 from '../assets/image4.jpg';
-import image5 from '../assets/image5.jpg';
-import image6 from '../assets/Project1.jpg';
-import image7 from '../assets/Project2.jpg';
+import image1 from '../assets/Project1.png';
+import image2 from '../assets/Project2.png';
+import image3 from '../assets/Project3.png';
+
 
 
 
@@ -13,39 +10,19 @@ export const projects = [
     title: "Skill Set",
     catagory: "Online Learning Platform",
     description: "Skill Set is a global online learning platform that offers anyone, anywhere, access to online courses and degrees from leading universities and companies.",
-    src: image6,
-    color: "#BBACAF"
+    src: image1,
   },
   {
-    title: "Clément Chapillon",
-    
-    catagory: "Online Learning Platform",
-    description: "An artistic exploration of photography and storytelling by Clément Chapillon, blending visuals with emotions.",
+    title: "LibraryHub",
+    catagory: "Book Library Management",
+    description: "LibraryHub is a personal book management system that lets you add, edit, and organize your library with ease—ideal for students, readers, and small libraries.",
     src: image2,
-    color: "#977F6D"
   },
   {
-    title: "Zissou",
-    
-    catagory: "Online Learning Platform",
-    description: "A vibrant and dynamic project inspired by the adventures of the Zissou crew, combining bold colors and imaginative visuals.",
+    title: "Pro Text Editor",
+    catagory: "Text Utility",
+    description: "Pro Text Editor is a versatile text utility tool that allows you to edit, format, and manipulate text with ease. It includes features like word count, character count, and more.",
     src: image3,
-    color: "#C2491D"
-  },
-  {
-    title: "Mathias Svold",
-    
-    catagory: "Online Learning Platform",
-    description: "A collaborative project by Mathias Svold and Ulrik Hasemann, capturing the essence of Nordic landscapes through stunning photography.",
-    src: image4,
-    color: "#B62429"
-  },
-  {
-    title: "Mark Rammers",
-    
-    catagory: "Online Learning Platform",
-    description: "A modern and minimalist portfolio by Mark Rammers, showcasing his expertise in architecture and design.",
-    src: image5,
-    color: "#88A28D"
   }
+
 ];
