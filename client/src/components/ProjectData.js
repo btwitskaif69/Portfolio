39e6f40a7371bc6e64aa0f54b6d3c280812689e3
Mohd Kaif <124128116+btwitskaif69@users.noreply.github.com>
2025-05-27 21:11:30 +0530

    added Sydney Events in the projects

--- client/src/components/ProjectData.js
+++ client/src/components/ProjectData.js
@@ -1,6 +1,8 @@
 import image1 from '../assets/Project1.jpg';
 import image2 from '../assets/Project2.jpg';
 import image3 from '../assets/Project3.jpg';
+import image4 from '../assets/Project4.jpg';
+
 
 
 
@@ -22,12 +24,20 @@ export const projects = [
     src: image2,
     techStack: ["Laravel", "Bootstrap", "Axios", "MongoDB"],
   },
+    {
+    title: "Sydney Events",
+    catagory: "Event Ticketing Platform",
+    description:
+      "Sydney Events is an event ticketing platform that allows users to discover, book, and manage tickets for various events happening in Sydney. It provides a seamless experience for both event organizers and attendees.",
+    src: image3,
+    techStack: ["React", "Tailwind CSS", "Node.js", "Express", "MongoDB", "RESTful API", "Puppeteer"],
+  },
   {
     title: "Pro Text Editor",
     catagory: "Text Utility",
     description:
       "Pro Text Editor is a versatile text utility tool that allows you to edit, format, and manipulate text with ease. It includes features like word count, character count, and more.",
-    src: image3,
+    src: image4,
     techStack: ["React", "Bootstrap"],
   },
 ];
