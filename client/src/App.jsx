c1137ea7abcfd6c05c5fc67fabe120213a3434fd
Mohd Kaif <124128116+btwitskaif69@users.noreply.github.com>
2025-06-25 23:23:13 +0530

    feat: remove unused imports and commented-out components in App.jsx for cleaner code

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,13 +4,10 @@ import { Hero } from './components/Hero'
 import { VelocityScroll } from "@/components/magicui/scroll-based-velocity";
 import { ContactForm } from '@/components/ContactForm';
 import Footer from './components/Footer';
-// import { ShineBorder } from "@/components/magicui/shine-border";
 import { TextReveal } from "@/components/magicui/text-reveal";
 import { SmoothCursor } from "@/components/ui/smooth-cursor";
 import FeaturedProjects from './components/FeaturedProjects';
 import SmoothScrollProvider from '@/components/SmoothScrollProvider';
-import Experience from './components/Experience';
-
 const App = () => {
 
   
@@ -25,11 +22,7 @@ const App = () => {
       <div className="pointer-events-none absolute inset-y-0 left-0 w-1/4 bg-gradient-to-r from-background"></div>
       <div className="pointer-events-none absolute inset-y-0 right-0 w-1/4 bg-gradient-to-l from-background"></div>
     </div>
-
       <FeaturedProjects/>
-      {/* <Experience/> */}
-      {/* <StackingCardsDemo/> */}
-
       <TextReveal>I am dedicated to transforming ideas into impactful realities, blending creativity, precision, and a deep passion for excellence to deliver work that truly makes a difference.</TextReveal>
       <ContactForm/>
       <Footer/>
