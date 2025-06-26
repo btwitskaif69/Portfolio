e5b811feccd8fa7d933ba3c2553580f7dfcfa78d
Mohd Kaif <124128116+btwitskaif69@users.noreply.github.com>
2025-06-26 22:19:11 +0530

    feat: adjust indentation in Navbar.jsx for improved code readability

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -6,15 +6,15 @@ import { ScrollProgress } from "@/components/magicui/scroll-progress";
 const Navbar = () => {
   return (
     <nav className="fixed top-0 left-0 w-full backdrop-blur-lg z-50">
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div className="flex justify-center items-center h-14 sm:h-18">
-          <img 
-            src={logo} 
-            alt="Logo"
-            className="h-5 sm:h-7 w-auto"
-          />
+        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+          <div className="flex justify-center items-center h-14 sm:h-18">
+            <img 
+              src={logo} 
+              alt="Logo"
+              className="h-5 sm:h-7 w-auto"
+            />
+          </div>
         </div>
-      </div>
       
       {/* Scroll Progress Bar */}
       <ScrollProgress className="absolute bottom-0" />
