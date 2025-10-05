2af4a6a8a9d335d45b28141967c31f5ceb39f773
Mohd Kaif <124128116+btwitskaif69@users.noreply.github.com>
2025-10-05 21:10:41 +0530

    UPDATED COMPONENT TO REDUCE LAG AND JITTER

--- client/src/components/SmoothScrollProvider.jsx
+++ client/src/components/SmoothScrollProvider.jsx
@@ -6,19 +6,29 @@ import Lenis from '@studio-freight/lenis';
 export default function SmoothScrollProvider({ children }) {
   useEffect(() => {
     const lenis = new Lenis({
-      duration: 1.2,
-      easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)), // custom easing
+      duration: 1.0,
+      easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
       smooth: true,
+      smoothTouch: false,
+      touchMultiplier: 2,
+      infinite: false,
+      syncTouch: false,
+      syncTouchLerp: 0.1,
     });
+
+    let rafId;
 
     function raf(time) {
       lenis.raf(time);
-      requestAnimationFrame(raf);
+      rafId = requestAnimationFrame(raf);
     }
 
-    requestAnimationFrame(raf);
+    rafId = requestAnimationFrame(raf);
 
     return () => {
+      if (rafId) {
+        cancelAnimationFrame(rafId);
+      }
       lenis.destroy();
     };
   }, []);
